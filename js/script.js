f1c3d85ba07b77956898a301be3a591373d696d8
Human Inhabitant <human-inhabitant@pm.me>
2020-08-24 18:35:29 -0400

    Completed s04...

--- js/script.js
+++ js/script.js
@@ -7,18 +7,28 @@
     .module( 'gitHubViewer' )
     .controller( 'MainController', ['$scope', '$http', function( $scope, $http ) {
 
+      const onRepos = function( response ) {
+        $scope.repos = response.data;
+      };
       const onUserComplete = function( response ) {
         $scope.user = response.data;
+        $http
+          .get( $scope.user.repos_url )
+          .then( onRepos, onError )
+        ;
       };
       const onError = function( reason ) {
-        $scope.error = 'Could not fetch the user.';
+        $scope.error = 'Could not fetch data.';
       };
-      $http
-        .get( 'https://api.github.com/users/timbl' )
-        .then( onUserComplete, onError )
-      ;
-
-      $scope.message = 'Hello, AngularJS...';
+      $scope.search = function( username ) {
+        $http
+          .get( `https://api.github.com/users/${username}` )
+          .then( onUserComplete, onError )
+        ;
+      };
+      $scope.username = 'timbl'; // carmelatroncoso qiangxue
+      $scope.message = 'GitHub Viewer';
+      $scope.repoSortOrder = '-stargazers_count';
 
     }]);
 })();
