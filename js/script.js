fe8d0ef5d64a6cdb9541fc2d2981d82f4ba93e1a
Human Inhabitant <human-inhabitant@pm.me>
2020-08-24 16:28:40 -0400

    Completed s03...

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 'use strict';
 
-const work = function() {
-  console.info( 'working hard...' );
-};
+(function() {
+  angular.module( 'gitHubViewer', [] );
 
-work();
+  angular
+    .module( 'gitHubViewer' )
+    .controller( 'MainController', ['$scope', '$http', function( $scope, $http ) {
+
+      const onUserComplete = function( response ) {
+        $scope.user = response.data;
+      };
+      const onError = function( reason ) {
+        $scope.error = 'Could not fetch the user.';
+      };
+      $http
+        .get( 'https://api.github.com/users/timbl' )
+        .then( onUserComplete, onError )
+      ;
+
+      $scope.message = 'Hello, AngularJS...';
+
+    }]);
+})();
 
 
